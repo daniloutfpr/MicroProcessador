687e66b7f14acda8dc9ff210244c9b2acb1f281a
vNemetz <vininemetz@gmail.com>
2025-09-17 19:17:33 -0300

    fix: compilation errors

--- ULA/ula.vhd
+++ ULA/ula.vhd
@@ -16,31 +16,31 @@ entity ALU is
         ent1 : in unsigned(15 downto 0);     -- Second 16 bits input
         sel_op : in unsigned(1 downto 0);    -- 4x (different operations)
 
-        output : out unsigned (15 downto 0); -- 16 bits output for the ALU
+        alu_out : out unsigned (15 downto 0); -- 16 bits output for the ALU
         --operation flags
         carry : out std_logic;
         zero : out std_logic;
         isNegative : out std_logic
-    )
-
+    );
+  end;
 architecture a_ALU of ALU is
 
-  signal result: unsigned (16 down to 0);
+  signal result: unsigned (16 downto 0);
 
   begin
     result <= (("0" & ent0) + ("0" & ent1)) when sel_op = "00" else  -- Sum operation
               (("0" & ent0) - ("0" & ent1)) when sel_op = "01" else  -- Subtraction operation
               (("0" & ent0) and ("0" & ent1)) when sel_op = "10" else  -- Comparsion operation (each bit)
               (("0" & ent0) or ("0" & ent1)) when sel_op = "11" else  -- Active bit checking (Or operation)
-              "00000000000000000"
+              "00000000000000000";
 
-    output <= result(15 down to 0);
+    alu_out <= result(15 downto 0);
     
     carry <= result(16);
 
-    zero <= '1' when result(15 down to 0) = (others => '0') else '0';
+    zero <= '1' when result(15 downto 0) = "0000000000000000" else '0';
 
-    isNegative < = output(15);
+    isNegative <= result(15);
 
   end architecture;
 
