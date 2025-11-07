c5478e46995c3f0cf9d1f05936297459c544868d
vNemetz <vininemetz@gmail.com>
2025-11-07 13:43:48 -0300

    doc: documents state machine

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -9,15 +9,20 @@ end entity;
 architecture a_fsm_state of fsm_state is
    signal state_s: unsigned(1 downto 0);
 begin
+
+   -- PROCESSOR STATES:
+   -- 00 = Instruction Fetch
+   -- 01 = Instruction Decode
+   -- 10 = Execute / Write back
    process(clock,reset)
    begin
       if reset='1' then
-         state_s <= "00";
+         state_s <= "00";           
       elsif rising_edge(clock) then
-         if state_s="10" then        -- se agora esta em 2
-            state_s <= "00";         -- o prox vai voltar ao zero
+         if state_s="10" then        -- if it's in the last state
+            state_s <= "00";         -- the next is the first again (00)
          else
-            state_s <= state_s+1;   -- senao avanca
+            state_s <= state_s+1;   -- else, goes to next state
          end if;
       end if;
    end process;
