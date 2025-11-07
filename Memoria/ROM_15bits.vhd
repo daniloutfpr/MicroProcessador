454dab904a1f7161a61d3bcb2f71692da6f9d0d6
vNemetz <vininemetz@gmail.com>
2025-11-07 13:32:42 -0300

    finished: lab6

--- Memoria/ROM_15bits.vhd
+++ Memoria/ROM_15bits.vhd
@@ -12,61 +12,61 @@ end entity;
 
 architecture a_rom of rom is
 
--- FORMATO: Op=[14:11], Rx=[10:7], Ry=[6:3], Imm/Jmp=[6:0]
+-- INSTRUCTION FORMAT: Op=[14:11], Rx=[10:7], Ry=[6:3], Imm/Jmp=[6:0]
     -- Opcodes: CLR=0001, MOV=0010, ADD=0011, SUB=0100, ADDI=0101
-    --          JMP=0110, BLS=0111
+    --          JMP=0110, BLS=0111, BPL = 1000
 
     type rom_array_type is array (0 to 127) of unsigned(14 downto 0);
 
     signal rom_memory : rom_array_type := (
-       -- A. Carrega R3 (o registrador 3) com o valor 0
+       -- A. Load R3 with 0 (clear)
         -- 0: CLR R3 (Op=0001, Rx=0011)
         0  => "0001" & "0011" & "0000000",
 
-        -- B. Carrega R4 com 0
+        -- B. Load R4 with 0 (clear)
         -- 1: CLR R4 (Op=0001, Rx=0100)
         1  => "0001" & "0100" & "0000000",
 
-        -- Setup (para Passo E): Carrega R8 com 29
-        -- (A condição "Se R3 < 30" é implementada como "Se R3 <= 29")
+        -- Setup (for step E): loads R8 with 29
+        -- (if R3 < 30 = if R3 <= 29)
         -- 2: CLR R8 (Op=0001, Rx=1000)
         2  => "0001" & "1000" & "0000000",
         -- 3: ADDI R8, 29 (Op=0101, Rx=1000, Imm=0011101)
         3  => "0101" & "1000" & "0011101",
 
-        -- C. Soma R3 com R4 e guarda em R4
-        -- (Este é o início do loop, no endereço 4)
+        -- C. Add R3 with R4 and stores the result in R4
+        -- (Loop begining (4th instruction))
         -- 4: LOOP: ADD R4, R3 (Op=0011, Rx=0100, Ry=0011)
         4  => "0011" & "0100" & "0011" & "000",
 
-        -- D. Soma 1 em R3
+        -- D. Add 1 in R3
         -- 5: ADDI R3, 1 (Op=0101, Rx=0011, Imm=0000001)
         5  => "0101" & "0011" & "0000001",
 
-        -- E. Se R3 < 30 (R3 <= 29) salta para a instrução do passo C (Addr 4)
-        -- Usamos R7 como temporário para R3 - R8
+        -- E. If R3 < 30 (R3 <= 29) branch to intruction C (Addr 4)
+        -- Use R7 as an aux for R3 - R8
         -- 6: MOV R7, R3 (Op=0010, Rx=0111, Ry=0011) 
         6  => "0010" & "0111" & "0011" & "000",
-        -- 7: SUB R7, R8 (Op=0100, Rx=0111, Ry=1000) -- R7 = R3-R8. SETA FLAGS (C, Z)
+        -- 7: SUB R7, R8 (Op=0100, Rx=0111, Ry=1000) -- R7 = R3-R8. Set flags (C, Z)
         7  => "0100" & "0111" & "1000" & "000",
-        -- 8: BLS -4 (Op=0111, Offset=1111100) -- Salta p/ 4 (PC=8, 8-4=4)
+        -- 8: BLS -4 (Op=0111, Offset=1111100) -- Branch to 4 (PC=8, 8-4=4)
         8  => "0111" & "0000" & "1111100",
 
-        -- F. Copia valor de R4 para R5
-        -- (Só executa quando o loop falha, ou seja, R3 = 30)
+        -- F. Copies R4 into R5
+        -- (Only executes in the end of the loop, so R3 = 30)
         -- 9: MOV R5, R4 (Op=0010, Rx=0101, Ry=0100)
         9  => "0010" & "0101" & "0100" & "000",
 
-        -- Fim. Trava o processador.
+        -- End. Stops the processor
         -- 10: FIM: JMP 10 (Op=0110, Addr=0001010)
         10 => "0110" & "0000" & "0001010",
 
-        -- Restante da memória é zero (NOP)
+        -- The rest of the memory is zero (NOP)
         others => (others => '0')
     );
 
 begin
-    -- Lógica de Leitura da ROM Síncrona
+    -- Syncronous ROM's logic
     process(clk)
     begin
         if rising_edge(clk) then
